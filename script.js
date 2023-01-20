eec7fcf5b3037c0e835eb3505174cbe219f57a30
mahagithab <121598922+mahagithab@users.noreply.github.com>
2023-01-20 10:45:21 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// Symbat Askarovaадмин
+// let textrySelector(".text")
+// let start = document.querySelector(".start")
+// let stop = document.querySelector(".stop")
+// let reset = document.querySelector(".reset")
+
+// let ss = 0
+// let mm = 0
+// let hh = 0
+// let startInterval 
+
+
+// let isStarted = true
+
+// const addZero = (num) => {
+//     if(num < 10){
+//         return `0${num}`
+//     } else{
+//         return num
+//     }
+// }
+
+
+
+
+// start.addEventListener("click", () => {
+//     if (isStarted){
+//         startInterval = setInterval(() => {
+//             ss++
+//             if (ss === 59) {
+//                 ss = 0
+//                 mm++
+//             }
+//             if (mm === 59) {
+//                 mm = 0
+//                 hh++
+//             }
+//             if (hh === 24) {
+//                 hh = 0
+//             }
+//             
+//             text.innerText = `${addZero(hh)}:${addZero(mm)}:${addZero(ss)}`
+//         }, 1000)
+//         isStarted = false
+//     }
+// })
+
+// stop.addEventListener("click", () => {
+//     isStarted = true
+//     clearInterval(startInterval)
+// })
+
+
+// reset.addEventListener("click", () => {
+//     isStarted = true
+//     text.innerText = "00:00:00"
+//     clearInterval(startInterval)
+//      ss = 0
+//      mm = 0
+//      hh = 0
+// }) = document.que
+
+
+
